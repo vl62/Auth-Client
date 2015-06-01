6969c3d2de7f7083aa273dd236ea58922a628230
owen <owen@host-153-155.ge.le.ac.uk>
2015-06-01 14:28:29 +0100

    [no commit message]

--- application/views/federated/auth/create_user.php
+++ application/views/federated/auth/create_user.php
@@ -19,7 +19,7 @@
 				<p>Please enter the users information below. Required fields are marked (*).</p>
 				<div id="infoMessage"><b><?php echo $message; ?></b></div>
 					
-				<?php echo form_open("auth/create_user"); ?>
+				<?php echo form_open("auth_federated/create_user"); ?>
 				<div class="row">
 					<label>ORCID:</label>
 				</div>
@@ -79,7 +79,7 @@
 					</select>
 				</p>
 					
-				<p><button type="submit" name="submit" class="btn btn-primary"><i class="icon-user icon-white"></i>  Create User</button><?php echo nbs(6); ?><button type="reset" class="btn"><i class="icon-remove-sign"></i> Clear</button><?php echo nbs(6); ?><a href="<?php echo base_url() . "auth/users"; ?>" class="btn" ><i class="icon-step-backward"></i> Go back</a></p>
+				<p><button type="submit" name="submit" class="btn btn-primary"><i class="icon-user icon-white"></i>  Create User</button><?php echo nbs(6); ?><button type="reset" class="btn"><i class="icon-remove-sign"></i> Clear</button><?php echo nbs(6); ?><a href="<?php echo base_url() . "auth_federated/users"; ?>" class="btn" ><i class="icon-step-backward"></i> Go back</a></p>
 			</div>
 		</div>
 	</div>
