28c6a51cff4d3e788fd74d3e1890976795f32add
dhiwatdg <dhiwatdg@host-203-241.eduroam-local.wifi.le.ac.uk>
2015-07-15 12:17:43 +0100

    [no commit message]

--- application/views/federated/auth/edit_user_network_groups.php
+++ application/views/federated/auth/edit_user_network_groups.php
@@ -24,21 +24,20 @@
 				<?php echo form_open("auth_federated/edit_user_network_groups", array('name' => 'editUser')); ?>
 	
 				<?php echo form_hidden(array('installation_key' => $this->config->item('installation_key'))); ?>	
-				<p>
 					<?php if (isset($groups)): ?>
 					<?php error_log(print_r($groups, 1)); ?>
 					Assign user to groups that given them openAccess to restrictedAccess records<br />(control click to select multiple): <br />
-                                
-                                <?php 
-						$group_count = count($groups) + 1;
-//						$curator_count = count($users) + 1;
-					?>
-					<select size="<?php echo $group_count; ?>" name="groups[]"  multiple="multiple">
-						<?php foreach ($groups as $group ): ?>
-							<option value="<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>" <?php if (isset($selected_groups)) { if (array_key_exists($group['id'], $selected_groups)) { echo 'selected="selected"'; }} ?>><?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?></option>
-						<?php endforeach; ?>
-					</select>
                                         
+                                <?php 
+                                        $group_count = count($groups) + 1;
+//                                        $curator_count = count($users) + 1;
+                                ?>
+                                <select size="<?php echo $group_count; ?>" name="groupsaa[]"  multiple="multiple">
+                                        <?php foreach ($groups as $group ): ?>
+                                                <option value="<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>" <?php if (isset($selected_groups)) { if (array_key_exists($group['id'], $selected_groups)) { echo 'selected="selected"'; }} ?>><?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?></option>
+                                        <?php endforeach; ?>
+                                </select>
+                                
                                 <h3>Master Network Groups</h3>
                                 <div class="row-fluid">
                                     <div class="span5 pagination-centered">
@@ -49,9 +48,8 @@
                                         <br><input type="button" value="&gt;&gt;"/><br><br>
                                         <input type="button" value="&lt;&lt;"/>
                                     </div>
-                                    <!--<h5>SELECTED</h5>-->
                                     <div class="span5 pagination-centered">
-                                        <select size="5" multiple id="mng_right">
+                                        <select size="5" multiple id="mng_right" name="groups[]" class="groupsSelected">
                                         </select>
                                     </div>
                                 </div>
@@ -67,7 +65,7 @@
                                         <input type="button" value="&lt;&lt;"/>
                                     </div>
                                     <div class="span5 pagination-centered">
-                                        <select size="5" multiple id="sdg_right">
+                                        <select size="5" multiple id="sdg_right" name="groups[]" class="groupsSelected">
                                         </select>
                                     </div>
                                 </div>
@@ -83,52 +81,64 @@
                                         <input type="button" value="&lt;&lt;"/>
                                     </div>
                                     <div class="span5 pagination-centered">
-                                        <select size="5" multiple id="cdg_left">
+                                        <select size="5" multiple id="cdg_right" name="groups[]" class="groupsSelected">
                                         </select>
                                     </div>
                                 </div>
                                 
-                                <script>
-                                <?php foreach ($groups as $group ): ?>
-                                        <?php if($group['group_type'] === "master"):?>
-                                            <?php if(array_key_exists($group['id'], $selected_groups)): ?> 
+                                
+                                <?php foreach ($groups as $group ):
+                                    if($group['group_type'] === "master"):
+                                        if(isset($selected_groups) && array_key_exists($group['id'], $selected_groups)): ?> 
+                                            <script type="text/javascript">
                                                 $("#mng_right").append($("<option></option>")
                                                 .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
                                                 .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
-                                            <?php else: ?>
+                                            </script>
+                                        <?php else: ?>
+                                            <script type="text/javascript">
                                                 $("#mng_left").append($("<option></option>")
                                                 .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
                                                 .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
-                                            <?php endif; ?>
-                                        <?php elseif ($group['group_type'] === "source_display"): ?>
-                                            <?php if(array_key_exists($group['id'], $selected_groups)): ?> 
+                                            </script>
+                                        <?php endif;
+                                    elseif ($group['group_type'] === "source_display"):
+                                        if(isset($selected_groups) && array_key_exists($group['id'], $selected_groups)): ?>
+                                            <script type="text/javascript">
                                                 $("#sdg_right").append($("<option></option>")
                                                 .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
                                                 .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
-                                            <?php else: ?>
+                                            </script>
+                                        <?php else: ?>
+                                            <script type="text/javascript">
                                                 $("#sdg_left").append($("<option></option>")
                                                 .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
                                                 .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
-                                            <?php endif; ?>
-                                        <?php elseif ($group['group_type'] === "count_display"): ?>    
-                                            <?php if(array_key_exists($group['id'], $selected_groups)): ?> 
+                                            </script>
+                                        <?php endif;
+                                    elseif ($group['group_type'] === "count_display"):
+                                        if(isset($selected_groups) && array_key_exists($group['id'], $selected_groups)): ?>
+                                            <script type="text/javascript">
                                                 $("#cdg_right").append($("<option></option>")
                                                 .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
                                                 .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
-                                            <?php else: ?>
+                                            </script>
+                                        <?php else: ?>
+                                            <script type="text/javascript">
                                                 $("#cdg_left").append($("<option></option>")
                                                 .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
                                                 .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
-                                            <?php endif; ?>
-                                        <?php endif; ?>
-                                <?php endforeach; ?>
-                                    </script>
+                                            </script>
+                                        <?php endif;
+                                    endif;
+                                endforeach; ?>
                                 
-					<?php else: ?>
-					<p><span class="label label-important">There are no network groups available to this installation. <br />A user will not be able to log in until they been assigned to at least one group.</span></p>
-					<?php endif; ?>
-				</p>
-                <br /><br /><br />	
+                                <?php else: ?>
+                                <p><span class="label label-important">There are no network groups available to this installation. <br />A user will not be able to log in until they been assigned to at least one group.</span></p>
+                                <?php endif; ?>
+                                
+                                <br /><br /><br />	
+                                
 				<?php echo form_hidden('id', $user_id); ?>
 				<?php echo form_hidden($csrf); ?>
 				<p><button type="submit" onclick="edit_user_network_groups();" name="submit" class="btn btn-primary"><i class="icon-user"></i>  Save</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url() . "auth_federated/users"; ?>" class="btn" ><i class="icon-step-backward"></i> Go back</a></p>
