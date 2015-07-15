340142f419cba4c4d7e24d02282958458ef1c43f
owen <owen@Air.home>
2015-07-15 16:05:04 +0100

    [no commit message]

--- application/helpers/cafevariome_helper.php
+++ application/helpers/cafevariome_helper.php
@@ -297,11 +297,6 @@ function prettyPrintJSON( $json ) {
     return $result;
 }
 
-function get_http_response_code($url) {
-    $headers = @get_headers($url);
-    return substr($headers[0], 9, 3);
-}
-
 function json_format($json) {
   if (!is_string($json)) {
     if (phpversion() && phpversion() >= 5.4) {
