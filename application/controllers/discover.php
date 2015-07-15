a7c033d3e964a9372bba7c711f56aee679a117fa
owen <owen@Air.home>
2015-07-15 15:54:35 +0100

    [no commit message]

--- application/controllers/discover.php
+++ application/controllers/discover.php
@@ -611,9 +611,8 @@ class Discover extends MY_Controller {
 						)
 					);
 					$context  = stream_context_create($opts);
-					$install_endpoint = $install_uri . "/discover_federated/variantcount/$term/$user_id/$network_key";
-
-					$all_counts_json = @file_get_contents($install_endpoint, false, $context);
+						
+					$all_counts_json = @file_get_contents($install_uri . "/discover_federated/variantcount/$term/$user_id/$network_key", false, $context);
 //					$all_counts_json = @file_get_contents($install_uri . "/discover_federated/variantcount/$term/$user_id/$network_key");
 //					error_log(print_r($http_response_header, 1));
 					error_log("all_counts_json -> $all_counts_json");
