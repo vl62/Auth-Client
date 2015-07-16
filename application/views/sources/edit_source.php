1c77767a0aa2c360d72aeb91015fd9a4ab438966
owen <owen@Air.home>
2015-07-16 20:14:28 +0100

    [no commit message]

--- application/views/sources/edit_source.php
+++ application/views/sources/edit_source.php
@@ -134,33 +134,9 @@
                                         <?php endif;
                                     endif;
                                 endforeach; ?>
-                                
-				<?php 
-					$group_count = count($groups) + 1;
-//					$curator_count = count($users) + 1;
-				?>
-				<select size="<?php echo $group_count; ?>" name=""  multiple="multiple">
-					<?php foreach ($groups as $group ): ?>
-						<?php
-							// Skip if it's a master group as we don't want users assigning master groups to sources (only users)
-							if ($group['group_type'] == 'master') {
-								continue;
-							}
-						?>
-						<option value="<?php echo $group['id'] . "," . $group['network_key']; ?>" <?php if ( isset($selected_groups)) { if (array_key_exists($group['id'], $selected_groups)) { echo 'selected="selected"'; } } ?>><?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?></option>
-					<?php endforeach; ?>
-				</select>
 				<?php endif; ?>
 			</p>
 			
-<!--			<p>
-				Edit Curators (control click to select multiple): <br />
-				<select size="<?php // echo $curator_count; ?>" name="curators[]"  multiple="multiple">
-					<?php // foreach ($users as $k => $user): ?>
-						<option value="<?php // echo $user->id; ?>" <?php // if (array_key_exists($user->id, $selected_curators)) { echo 'selected="selected"'; } ?>><?php // echo $user->username; ?></option>
-					<?php // endforeach; ?>
-				</select>						   
-			</p>-->
 			
                         <p><button type="submit" onclick="edit_source_assign_groups()" name="submit" class="btn btn-primary"><i class="icon-file"></i>  Save Source</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url() . "sources/sources";?>" class="btn" ><i class="icon-step-backward"></i> Go back</a></p>
 		
