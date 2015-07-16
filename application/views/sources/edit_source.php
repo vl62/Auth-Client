50d4ad34bb753f893bd4e92c35296d9962b8ed98
dhiwatdg <dhiwatdg@host-203-241.eduroam-local.wifi.le.ac.uk>
2015-07-16 09:52:10 +0100

    [no commit message]

--- application/views/sources/edit_source.php
+++ application/views/sources/edit_source.php
@@ -70,11 +70,76 @@
 				<p><span class="label label-important">There are no network groups available to this installation. <br /></span></p>
 				<?php else: ?>
 				Edit Groups Allowed to Access restrictedAccess Variants<br />(control click to select multiple): <br />
+                                
+                                <h3>Source Display Groups</h3>
+                                <div class="row-fluid">
+                                    <div class="span5 pagination-centered">
+                                        <select size="5" multiple id="sdg_left">
+                                        </select>
+                                    </div>
+                                    <div class="span2 pagination-centered">
+                                        <br><input type="button" value="&gt;&gt;"/><br><br>
+                                        <input type="button" value="&lt;&lt;"/>
+                                    </div>
+                                    <div class="span5 pagination-centered">
+                                        <select size="5" multiple id="sdg_right" name="groups[]" class="groupsSelected">
+                                        </select>
+                                    </div>
+                                </div>
+                                
+                                <h3>Count Display Groups</h3>
+                                <div class="row-fluid">
+                                    <div class="span5 pagination-centered">
+                                        <select size="5" multiple id="cdg_left">
+                                        </select>
+                                    </div>
+                                    <div class="span2 pagination-centered">
+                                        <br><input type="button" value="&gt;&gt;"/><br><br>
+                                        <input type="button" value="&lt;&lt;"/>
+                                    </div>
+                                    <div class="span5 pagination-centered">
+                                        <select size="5" multiple id="cdg_right" name="groups[]" class="groupsSelected">
+                                        </select>
+                                    </div>
+                                </div>
+                                
+                                <?php foreach ($groups as $group ):
+                                    if ($group['group_type'] === "source_display"):
+                                        if(isset($selected_groups) && array_key_exists($group['id'], $selected_groups)): ?>
+                                            <script type="text/javascript">
+                                                $("#sdg_right").append($("<option></option>")
+                                                .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
+                                                .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
+                                            </script>
+                                        <?php else: ?>
+                                            <script type="text/javascript">
+                                                $("#sdg_left").append($("<option></option>")
+                                                .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
+                                                .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
+                                            </script>
+                                        <?php endif;
+                                    elseif ($group['group_type'] === "count_display"):
+                                        if(isset($selected_groups) && array_key_exists($group['id'], $selected_groups)): ?>
+                                            <script type="text/javascript">
+                                                $("#cdg_right").append($("<option></option>")
+                                                .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
+                                                .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
+                                            </script>
+                                        <?php else: ?>
+                                            <script type="text/javascript">
+                                                $("#cdg_left").append($("<option></option>")
+                                                .attr("value",'<?php echo $group['id'] . "," . $group['network_key'] . ""; ?>')
+                                                .text('<?php echo $group['description'] . " (Network:" . $group['network_name'] . ")"; ?>')); 
+                                            </script>
+                                        <?php endif;
+                                    endif;
+                                endforeach; ?>
+                                
 				<?php 
 					$group_count = count($groups) + 1;
 //					$curator_count = count($users) + 1;
 				?>
-				<select size="<?php echo $group_count; ?>" name="groups[]"  multiple="multiple">
+				<select size="<?php echo $group_count; ?>" name=""  multiple="multiple">
 					<?php foreach ($groups as $group ): ?>
 						<?php
 							// Skip if it's a master group as we don't want users assigning master groups to sources (only users)
@@ -97,7 +162,7 @@
 				</select>						   
 			</p>-->
 			
-			<p><button type="submit" name="submit" class="btn btn-primary"><i class="icon-file"></i>  Save Source</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url() . "sources/sources";?>" class="btn" ><i class="icon-step-backward"></i> Go back</a></p>
+                        <p><button type="submit" onclick="edit_source_assign_groups()" name="submit" class="btn btn-primary"><i class="icon-file"></i>  Save Source</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="<?php echo base_url() . "sources/sources";?>" class="btn" ><i class="icon-step-backward"></i> Go back</a></p>
 		
 			</div>
 		</div>
