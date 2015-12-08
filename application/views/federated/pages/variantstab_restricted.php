6eceac8795516bb569403e8a77af39ad1d7a312a
achrys <achrys@localhost>
2015-12-08 17:23:16 +0000

    epad_v1

---
+++ application/views/federated/pages/variantstab_restricted.php
@@ -0,0 +1,29 @@
+<?php
+
+
+echo '<div style="width:800px; margin:0 auto;">';
+
+foreach ($query->result_array() as $row)
+{
+   echo '<em><strong>Source owner name: ';
+   echo $row['owner_name'];
+   echo "</BR>";
+   echo 'Source owner email: ';
+   echo $row['email'];
+   echo '</em></strong>';
+   echo "</BR>";
+}
+
+echo "</BR>";
+
+echo $this->input->post('email');
+
+echo 'List of Derids: </BR>';
+foreach ($variants as $variant) {
+    echo $variant;
+    echo '</BR>';
+}
+        
+
+echo '</div>';
+?>
