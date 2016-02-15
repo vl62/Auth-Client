5bc3e36975bf0aab6c436f5e6c54cbf8495a91d6
colinveal <colinveal@localhost>
2016-02-15 11:53:48 +0000

    fix for red xs when user is not allowed to see derids

--- application/views/pages/sources_table.php
+++ application/views/pages/sources_table.php
@@ -136,8 +136,14 @@ $(function (){
 							
 							<?php if ( array_key_exists('restrictedAccess', $count )): ?>
 									<?php if(!$view_derids): ?>
+										<?php if ( $count['restrictedAccess'] === 0 ): ?>
+											<!-- <?php echo "0"; ?> -->
+											<a rel="popover" data-content="Sorry, there are no records of this type available." data-original-title="Access Records"> <?php echo img(base_url('resources/images/cafevariome/cross.png'));?></a>
+										<?php else: ?>
+											<a href="#" rel="popover" data-content="The display of DerIDs has been limited to specific users for this source." data-original-title="Unable to view DerIDs"><i class="fa fa-ban fa-2x"></i></a>
+										<?php endif; ?>
 										<!-- <a rel="popover" data-content="Sorry, there are no records of this type available." data-original-title="Access Records"> <?php echo img(base_url('resources/images/cafevariome/cross.png'));?></a> -->
-										<a href="#" rel="popover" data-content="The display of DerIDs has been limited to specific users for this source." data-original-title="Unable to view DerIDs"><i class="fa fa-ban fa-2x"></i></a>
+										
 									<?php elseif ( $source_types[$source] == "central" ): ?>
 										<?php echo anchor("http://www.cafevariome.org/discover/variants/$term/" . $central_source[$source] . "/openAccess", img(array('src' => base_url('resources/images/cafevariome/cafevariome_node.png'),'border'=>'0','alt'=>'Request Data')),array('class'=>'imglink', 'target' => '_blank', 'rel' => "popover", 'data-content' => "Click to access these records in Cafe Variome Central. N.B. All access control to these records is controlled by Cafe Variome Central.", 'data-original-title' => "Access CV Central Records")); ?>
 									<?php elseif ( $source_types[$source] == "federated" ): ?>
