c117baa5af54b80cc237ba0bffccfc9e3f2b19d4
dhiwatdg <dhiwatdg@localhost>
2016-04-20 18:36:54 +0100

    updated derids view header

--- application/views/federated/pages/variantstab_restricted.php
+++ application/views/federated/pages/variantstab_restricted.php
@@ -1,29 +1,58 @@
+<div style="width:800px; margin:0 auto;">
+	
+	
+	<table>
+		<tr>
+			<td><em>Executed by</em></td>
+			<td><em style="padding-right: 10px;">:</em></td>
+			<td><em><strong><?php echo $this->session->userdata('email') . " &#64; " . date("Y-m-d H:i:s") . " GMT"; ?></strong></em></td>
+		</tr>
+		<tr>
+			<td><em>Query source</em></td>
+			<td><em style="padding-right: 10px;">:</em></td>
+			<td><em><strong><?php echo $source; ?></strong></em></td>
+		</tr>
+	<?php if(isset($precan_log_id)): ?>
+		<tr>
+			<td><em>Query type</em></td>
+			<td><em style="padding-right: 10px;">:</em></td>
+			<td><em><strong><?php echo $api['case_control'] ?></strong></em></td>
+		</tr>
+		<tr>
+			<td><em>Query notes</em></td>
+			<td><em style="padding-right: 10px;">:</em></td>
+			<td style="width: 550px;"><em><strong><?php echo $api['notes'] ? $api['notes'] : "n/a"; ?></strong></em></td>
+		</tr>
+		<tr>
+			<td><em>Query syntax</em></td>
+			<td><em style="padding-right: 10px;">:</em></td>
+			<td style="width: 550px;"><em><strong><?php echo $api['queryString']; ?></strong></em></td>
+		</tr>
+	<?php else: ?>
+		<tr>
+			<td><em>Query syntax</em></td>
+			<td><em style="padding-right: 10px;">:</em></td>
+			<td style="width: 550px;"><em><strong><?php echo $term; ?></strong></em></td>
+		</tr>
+	<?php endif; ?>
+		
+		<tr>
+			<td><em>Total results</em></td>
+			<td><em style="padding-right: 10px;">:</em></td>
+			<td><em><strong><?php echo count($variants); ?></strong></em></td>
+		</tr>
+	</table>
+	
+
 <?php
+	echo "<br/>";
+	echo $this->input->post('email');
 
-
-echo '<div style="width:800px; margin:0 auto;">';
-
-foreach ($source_owner as $row)
-{
-   echo '<em><strong>Source owner name: ';
-   echo $row['owner_name'];
-   echo "</BR>";
-   echo 'Source owner email: ';
-   echo $row['email'];
-   echo '</em></strong>';
-   echo "</BR>";
-}
-
-echo "</BR>";
-
-echo $this->input->post('email');
-
-echo 'List of DerIDs: </BR>';
-foreach ($variants as $variant) {
-    echo $variant;
-    echo '</BR>';
-}
-        
+	echo '<em><strong><u>List of DerIDs</u>: </strong></em><br/>';
+	foreach ($variants as $variant) {
+	    echo $variant;
+	    echo '<br/>';
+	}
 
 echo '</div>';
 ?>
