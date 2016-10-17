2c988491a4ddb3b1e1ef5d98f1770c573a01e5c9
colinveal <colinveal@localhost>
2016-10-17 16:38:03 +0100

    exclude records

--- application/views/pages/exclude_records.php
+++ application/views/pages/exclude_records.php
@@ -10,11 +10,11 @@ textarea {
 <div class="container">
 	<h3>Submit the Records IDs to EXCLUDE them from the Study</h3>
 
-	<form>
+	<form method="post">
 		<textarea name="content"></textarea> <br>
 
 		<h3>Reason for exclusion (optional)</h3>
-  		<textarea name="reason" id="reason" style="width: 700px; height: 200px;" maxlength="12"></textarea><br>
+  		<textarea name="reason" id="reason" style="width: 700px; height: 200px;" maxlength="512"></textarea><br>
 		<input type="submit" value="Submit"> <em>(max. 512 characters)</em>
 	</form> <br> <br>
 
